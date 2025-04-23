e7de5ae9310de32a7ff2adbcfd39b7366170c320
Bách Phạm <bqpham24@earlham.edu>
2025-04-23 03:14:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HES/', // Set this to your GitHub repo name with slashes
+  base: '/', // Root path for custom domain
   build: {
     outDir: 'dist' // This is Vite's default, make sure it matches your deploy script
   }
